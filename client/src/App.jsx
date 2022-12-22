da66b4bd355ede628530df90a914a984cfd8ac35
Stephan Griesel <sgriesel@gmail.com>
2022-12-22 07:03:20 +0100

    set up

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex justify-center align-middle h-screen">
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default App
