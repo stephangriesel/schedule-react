4905f4ff6ad4722acd7baa7911ed5e81d752c5fe
Stephan Griesel <sgriesel@gmail.com>
2022-12-23 06:35:20 +0100

    import components and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import BookUser from './components/BookUser';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Signup from './components/Signup';
+
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="flex justify-center align-middle h-screen">
-      <h1>hello</h1>
-    </div>
-  )
-}
+      <div>
+          <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<Login />} />
+                  <Route path='/register' element={<Signup />} />
+                  <Route path='/dashboard' element={<Dashboard />} />
+                  <Route path='/profile/:id' element={<Profile />} />
+                  <Route path='/book/:user' element={<BookUser />} />
+              </Routes>
+          </BrowserRouter>
+          <ToastContainer />
+      </div>
+  );
+};
 
 export default App
