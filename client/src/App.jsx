6c9e2a9de59a6a5b41a91f564dba6dc3713bf19d
Stephan Griesel <sgriesel@gmail.com>
2022-12-27 07:30:22 +0100

    remove old styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,25 +8,23 @@ import Profile from './components/Profile';
 import Signup from './components/Signup';
 
 import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
-
-import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-      <div>
-          <BrowserRouter>
-              <Routes>
-                  <Route path='/' element={<Login />} />
-                  <Route path='/register' element={<Signup />} />
-                  <Route path='/dashboard' element={<Dashboard />} />
-                  <Route path='/profile/:id' element={<Profile />} />
-                  <Route path='/book/:user' element={<BookUser />} />
-              </Routes>
-          </BrowserRouter>
-          <ToastContainer />
-      </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Signup />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/profile/:id' element={<Profile />} />
+          <Route path='/book/:user' element={<BookUser />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </div>
   );
 };
 
-export default App
+export default App;
