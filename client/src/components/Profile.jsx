895fa08ad8f6cbd1d4a0dd57c4702fce986ea1da
Stephan Griesel <sgriesel@gmail.com>
2022-12-28 07:17:22 +0100

    profile mock

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { id } = useParams();
 
-export default Profile
+  return (
+    <main className="profile">
+      <div className="w-2/3">
+        <h2>Hey</h2>
+        <p>Here is your schedule: </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>MON</td>
+              <td>8:00am</td>
+              <td>10:00pm</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
