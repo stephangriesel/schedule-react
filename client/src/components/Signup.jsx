a07b913b87379827cdefaed165aedb2e9f12a15f
Stephan Griesel <sgriesel@gmail.com>
2022-12-28 07:17:45 +0100

    util & prettier

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,73 +1,74 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { handleRegister } from "../utils/resource";
+import { useNavigate, Link } from "react-router-dom";
 
 const Signup = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     if (username.trim() && password.trim() && email.trim()) {
-      console.log(email, username, password);
-      setPassword('');
-      setUsername('');
-      setEmail('');
+      handleRegister(email, username, password, navigate);
+      setPassword("");
+      setUsername("");
+      setEmail("");
     }
   };
 
   return (
-    <main className='flex justify-center items-center h-screen rounded p-10'>
+    <main className="flex justify-center items-center h-screen rounded p-10">
       <form
-        className='flex flex-col bg-slate-300 rounded p-2'
+        className="flex flex-col bg-slate-300 rounded p-2"
         onSubmit={handleSubmit}
       >
-        <h2 className='font-bold m-5'>Create an account</h2>
-        <label htmlFor='email' className='m-2'>
+        <h2 className="font-bold m-5">Create an account</h2>
+        <label htmlFor="email" className="m-2">
           Email Address
         </label>
         <input
-          className='p-3'
-          id='email'
-          name='email'
-          type='email'
+          className="p-3"
+          id="email"
+          name="email"
+          type="email"
           required
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={e => setEmail(e.target.value)}
         />
-        <label className='m-2' htmlFor='username'>
+        <label className="m-2" htmlFor="username">
           Username
         </label>
         <input
-          className='username p-3'
-          id='username'
-          name='username'
+          className="username p-3"
+          id="username"
+          name="username"
           required
-          type='text'
+          type="text"
           value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={e => setUsername(e.target.value)}
         />
-        <label className='m-2' htmlFor='password'>
+        <label className="m-2" htmlFor="password">
           Password
         </label>
         <input
-          className='password p-3'
-          id='password'
-          type='password'
-          name='password'
+          className="password p-3"
+          id="password"
+          type="password"
+          name="password"
           required
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={e => setPassword(e.target.value)}
         />
-        <button className='signupButton p-2 m-5 transition ease-in-out delay-150 bg-green-500 hover:bg-green-600 duration-300 rounded'>
+        <button className="signupButton p-2 m-5 transition ease-in-out delay-150 bg-green-500 hover:bg-green-600 duration-300 rounded">
           REGISTER
         </button>
-        <p className='text-sm m-4'>
-          Already have an account?{' '}
+        <p className="text-sm m-4">
+          Already have an account?{" "}
           <Link
-            className='link underline decoration-4 decoration-green-500 hover:decoration-green-600'
-            to='/'
+            className="link underline decoration-4 decoration-green-500 hover:decoration-green-600"
+            to="/"
           >
             Sign in
           </Link>
