5cfa5ef70121ba6080f2022c7fccd604a720c8da
Stephan Griesel <sgriesel@gmail.com>
2022-12-29 06:16:03 +0100

    get schedules

--- server/index.js
+++ server/index.js
@@ -62,10 +62,29 @@ app.post("/login", (req, res) => {
 // create new schedule
 app.post("/schedule/create", (req, res) => {
   const { userId, timezone, schedule } = req.body;
+  console.log("create scheduled request: ",req.body)
   // filters the database via the id
   let result = database.filter(db => db.id === userId);
   // updates the user's schedule and timezone
   result[0].timezone = timezone;
   result[0].schedule = schedule;
   res.json({ message: "OK" });
+});
+
+// get schedules
+app.get("/schedules/:id", (req, res) => {
+  const { id } = req.params;
+  // filters the array via the ID
+  let result = database.filter((db) => db.id === id);
+  // returns the schedule, time and username
+  if (result.length === 1) {
+      return res.json({
+          message: "Schedules successfully retrieved!",
+          schedules: result[0].schedule,
+          username: result[0].username,
+          timezone: result[0].timezone,
+      });
+  }
+  // if user not found
+  return res.json({ error_message: "Sign in again, an error occured..." });
 });
