d52edc875b8bfb803c8cf9dc78f0f97d2f1a3644
Stephan Griesel <sgriesel@gmail.com>
2022-12-29 06:36:06 +0100

    fetchbooking function & logic

--- client/src/components/BookUser.jsx
+++ client/src/components/BookUser.jsx
@@ -1,54 +1,73 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const BookUser = () => {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const { user } = useParams();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, fullName, message);
-    setFullName("");
-    setMessage("");
+    setFullName('');
+    setMessage('');
   };
 
+  const [schedules, setSchedules] = useState([]);
+  const [timezone, setTimezone] = useState('');
+  const [error, setError] = useState(false);
+  const [receiverEmail, setReceiverEmail] = useState('');
+
+  useEffect(() => {
+    fetchBookingDetails(
+      user,
+      setError,
+      setTimezone,
+      setSchedules,
+      setReceiverEmail
+    );
+  }, [user]);
+
+  if (error) {
+    return <ErrorPage error="User doesn't exist" />;
+  }
+
   return (
-    <div className="bookContainer">
-      <h2 className="bookTitle">Book a session with {user}</h2>
-      <form onSubmit={handleSubmit} className="booking__form">
-        <label htmlFor="fullName">Full Name</label>
+    <div className='bookContainer'>
+      <h2 className='bookTitle'>Book a session with {user}</h2>
+      <form onSubmit={handleSubmit} className='booking__form'>
+        <label htmlFor='fullName'>Full Name</label>
         <input
-          id="fullName"
-          name="fullName"
-          type="text"
+          id='fullName'
+          name='fullName'
+          type='text'
           required
           value={fullName}
-          onChange={e => setFullName(e.target.value)}
+          onChange={(e) => setFullName(e.target.value)}
         />
-        <label htmlFor="email">Email Address</label>
+        <label htmlFor='email'>Email Address</label>
         <input
-          id="email"
-          name="email"
+          id='email'
+          name='email'
           required
-          type="email"
+          type='email'
           value={email}
-          onChange={e => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
-        <label htmlFor="message">Any important note? (optional)</label>
+        <label htmlFor='message'>Any important note? (optional)</label>
         <textarea
           rows={5}
-          name="message"
-          id="message"
+          name='message'
+          id='message'
           value={message}
-          onChange={e => setMessage(e.target.value)}
+          onChange={(e) => setMessage(e.target.value)}
         />
 
-        <label htmlFor="session">Select your preferred session</label>
+        <label htmlFor='session'>Select your preferred session</label>
 
-        <button className="bookingBtn">SEND</button>
+        <button className='bookingBtn'>SEND</button>
       </form>
     </div>
   );
