b1734bc1c25f971d4cac7d6886a3932769ac83c8
Stephan Griesel <sgriesel@gmail.com>
2023-01-04 06:39:55 +0100

    user routes add be

---
+++ routes/userRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.post('/login', async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
+app.use(express.json());
+const userRoute = require('./routes/userRoute');
+
+app.use('/api/user', userRoute);
 const port = process.env.PORT || 5070;
 
 console.log('⚡ Your DB connection details: ', process.env.MONGO_URL);
